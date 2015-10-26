5e0355463272ea62f09180e5431e7dc43e144a3d
Everett C. Haimes III <everett@powernode.net>
2015-10-26 14:38:39 -0800

    Only set admin_view ability if user signed in.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,7 +41,7 @@ class ApplicationController < ActionController::Base
   def set_credentials
     @current_user = current_user
     @current_account = user_signed_in? ? (Account.accessible_by(Ability.new(user: @current_user)).find_by(id: session[:account_id]) || @current_user.account) : Account.new
-    @current_ability = Ability.new(user: @current_user, account: @current_account, admin_view: @current_user.preferences['admin_view'])
+    @current_ability = Ability.new(user: @current_user, account: @current_account, admin_view: user_signed_in? ? @current_user.preferences['admin_view'] : false)
   end
 
   def set_header
