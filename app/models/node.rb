0e6fd8b57ab8dabc83be66644fa478b512e7a1fe
Everett C. Haimes III <everett@powernode.net>
2015-12-12 15:27:10 -0900

    Initialize ssh keys before create, rearrange node instance controls.

--- app/models/node.rb
+++ app/models/node.rb
@@ -43,8 +43,7 @@ class Node < ActiveRecord::Base
   validates_uniqueness_of :name, scope: :account_id
   validate  :enforce_limits, on: :create
 
-  after_initialize :initialize_ssh_keys
-
+  before_create :initialize_ssh_keys
   before_destroy { |n| n.node_instances.cloud_variety.size == 0 && n.node_instances.dynamic_variety.size == 0 }
   before_save :destroy_invalid_associations, if: :node_template_id_changed?
 
